36831ecd52044d659ea09686638d3cc866bc66cf
Ayushman <ayushman11022002@gmail.com>
2021-09-29 16:36:34 +0530

    add salt and hash password

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose= require('mongoose');
 const {isEmail}= require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema= new mongoose.Schema({
     email: {
@@ -14,6 +15,19 @@ const userSchema= new mongoose.Schema({
         required: [true,'Please enter a password'],
         minlength: [6,'Minimum passowrd length is 6 characters']
     }
+})
+
+// fire a fuction after doc is saved to db
+userSchema.post('save', function(doc, next) {
+    console.log('new user created', doc);
+    next();
+});
+
+// fire a function before doc is saved to db
+userSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt() // this refers to the user instance created by User.create()
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
 })
 
 const User= mongoose.model('user', userSchema);
